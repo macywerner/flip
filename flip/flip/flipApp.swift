c7787b8b57fee7ee60514619c665dd15507d9db8
Macy Werner <mwerner@chapman.edu>
2021-11-30 13:21:17 -0800

    xcode file created
    first commit

---
+++ flip/flip/flipApp.swift
@@ -0,0 +1,17 @@
+//
+//  flipApp.swift
+//  flip
+//
+//  Created by cpsc on 11/30/21.
+//
+
+import SwiftUI
+
+@main
+struct flipApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
